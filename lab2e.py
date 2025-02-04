c84030f79ccc51a182900b286b0cbc5d8f3ff58f
Dayaz-ops <daniyalayaz@yahoo.ca>
2025-02-04 18:57:56 -0500

    Add files via upload

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Daniyal Ayaz
+# Author ID: 111408167
+# Date Created: 2025/02/04
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
