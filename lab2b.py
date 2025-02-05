8ad8b5c787dd764b88545dcefd2c3b7e26318eef
Dayaz-ops <daniyalayaz@yahoo.ca>
2025-02-05 16:12:59 -0500

    Delete lab2b.py

--- lab2b.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-name = input("Name: ")  # Prompt user for name
-age = input("Age: ")  # Prompt user for age
-
-print(f'Hi {name}, you are {age} years old.')  # Print formatted output
-
-
